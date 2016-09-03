b8934a2666254a0ad7b3401266ab7b73affe8220
Thomas J. Shavor <thomasshavor@gmail.com>
2016-09-03 07:15:37 -0500

    prototypal inhertiance chain revised

--- javascripts/quiz.js
+++ javascripts/quiz.js
@@ -12,12 +12,84 @@ battledome.Combatants.Mecha = function() {
 
 //set top of prototype chain (Mecha constructor)
 
-    this.health = 50;
-    this.damage = 3;
-    this.allowedModels = ["Gunen_Lagann", "Gundam_Unicorn", ""];
+    this.health = 0;
+    this.damageBonus = 0;
+    this.allowedModels = ["Gunen_Lagann", "Gundam_Unicorn", "Scout", "Vulture", "Rocketeer", "Brawler"];
     this.weapon = null;
 };
 });
+
+//next level down on chain: types (Bipedal/Drone/Combat Constructors)
+battledome.Combatants.Bipedal = function() {
+  this.health = 20;
+  this.damageBonus= 10
+};
+battledome.Combatants.Bipedal.prototype = new Battledome.Combatants.Mecha();
+
+battledome.Combatants.Drone = function() {
+  this.name = "Drone";
+  this.health = this.healthBonus +5;
+  this.damageBonus = this.damageBonus +5;
+};
+battledome.Combatants.Drone.prototype = new Battledome.Combatants.Mecha();
+
+battledome.Combatants.Combat = function() {
+  this.name = "Combat";
+  this.health = this.healthBonus + 10;
+  this.damageBonus = this.damageBonus +20;
+};
+battledome.Combatants.Combat.prototype = new Battledome.Combatants.Mecha();
+
+//Designate last levels of prototype chain (2 "models" for each class of Mecha)
+//Bipedal:
+battledome.Combatants.Gunen_Lagann = function() {
+  this.name = userName;
+  this.healthBonus = this.healthBonus + Math.floor(Math.random() * 6 + 1);
+  this.damageBonusBonus = this.damageBonusBonus + Math.floor(Math.random() * 8 + 1);
+};
+battledome.Combatants.Gunen_Lagann.prototype = new battledome.Combatants.Bipedal();
+
+battledome.Combatants.Gundam_Unicorn = function() {
+  this.name = userName;
+  this.healthBonus = this.healthBonus + Math.floor(Math.random() * 3 + 1);
+  this.damageBonusBonus = this.damageBonusBonus + Math.floor(Math.random() * 4 + 7);
+};
+battledome.Combatants.Gundam_Unicorn.prototype = new battledome.Combatants.Bipedal();
+////////////////////////////////////////////////////////////////////////////////////
+// Drone:
+Battledome.Combatants.Scout = function() {
+  this.name = "Scout";
+  this.healthBonus = this.healthBonus + Math.floor(Math.random() * 3 + 1);
+  this.damageBonusBonus = this.damageBonusBonus +Math.floor(Math.random() * 3 + 1);
+};
+Battledome.Combatants.Scout.prototype = new Battledome.Combatants.Drone();
+
+Battledome.Combatants.Vulture = function() {
+  this.name = "Vulture";
+  this.healthBonus = this.healthBonus + Math.floor(Math.random() * 3 + 1);
+  this.damageBonus = this.damageBonus + Math.floor(Math.random() * 3 + 1);
+};
+Battledome.Combatants.Vulture.prototype = new Battledome.Combatants.Drone();
+///////////////////////////////////////////////////////////////////////////
+// Combat:
+Battledome.Combatants.Rocketeer = function() {
+  this.name = "Rocketeer";
+  this.healthBonus = this.healthBonus + Math.floor(Math.random() * 3 + 1);
+  this.damageBonus = this.damageBonus + Math.floor(Math.random() * 3 + 1);
+};
+Battledome.Combatants.Rocketeer.prototype = new Battledome.Combatants.Combat();
+
+Battledome.Combatants.Brawler = function() {
+  this.name = "Brawler";
+  this.healthBonus = this.healthBonus + Math.floor(Math.random() * 3 + 1);
+  this.damageBonus = this.damageBonus + Math.floor(Math.random() * 3 + 1);
+};
+Battledome.Combatants.Brawler.prototype = new Battledome.Combatants.Combat();
+
+
+
+
+
 
 var playerOne = "";
 // var playerOne = new Battledome.Combatants.Mecha();
@@ -47,57 +119,40 @@ playerOne.playerName= $("#player-name1").val();
 // ////event handlers for classes///////
 // $("#Gunen_Lagann").click(function(){
 //   console.log("you picked the Gunen_Lagann");
-//   playerOne.class = new Batledrone.Arena.Gunen_Lagann();
+//   playerOne.class = new Batledrone.Combatants.Gunen_Lagann();
 //   console.log(playerOne);
 // })
 
 // $("#Gundam_Unicorn").click(function(){
 //   console.log("you picked the Gundam_Unicorn");
-//   playerOne.class = new Batledrone.Arena.Gundam_Unicorn;
-//   console.log(playerOne);
-// })
-
-// $("#Gundam_Seed").click(function(){
-//   console.log("you picked the Gundam_Seed");
-//   playerOne.class = new Batledrone.Arena.Gundam_Seed;
+//   playerOne.class = new Batledrone.Combatants.Gundam_Unicorn;
 //   console.log(playerOne);
 // })
 
 // $("#Scout").click(function(){
 //   console.log("you picked the Scout");
-//   playerOne.class = new Batledrone.Arena.Scout;
+//   playerOne.class = new Batledrone.Combatants.Scout;
 //   console.log(playerOne);
 // })
 
 // $("#Vulture").click(function(){
 //   console.log("you picked the Vulture");
-//   playerOne.class = new Batledrone.Arena.Vulture;
-//   console.log(playerOne);
-// })
-
-// $("#Technician").click(function(){
-//   console.log("you picked the Technician");
-//   playerOne.class = new Batledrone.Arena.Technician;
+//   playerOne.class = new Batledrone.Combatants.Vulture;
 //   console.log(playerOne);
 // })
 
 // $("#Rocketeer").click(function(){
 //   console.log("you picked the Rocketeer");
-//   playerOne.class = new Batledrone.Arena.Rocketeer;
+//   playerOne.class = new Batledrone.Combatants.Rocketeer;
 //   console.log(playerOne);
 // })
 
 // $("#Brawler").click(function(){
 //   console.log("you picked the Brawler");
-//   playerOne.class = new Batledrone.Arena.Brawler;
+//   playerOne.class = new Batledrone.Combatants.Brawler;
 //   console.log(playerOne);
 // })
 
-// $("#Berserker").click(function(){
-//   console.log("you picked the Berserker");
-//   playerOne.class = new Batledrone.Arena.Berserker;
-//   console.log(playerOne);
-// })
 
 
 // $(document).ready(function() {
@@ -163,21 +218,21 @@ playerOne.playerName= $("#player-name1").val();
 //     // var foe = new Batledrone.Combatants.Monster();
 //     // console.log("spell: ", spell.toString());
 //     // console.log("player one health at start: ", playerHealth);
-//     var player1Damage = playerOne.weapon.attackDamage();
-//     console.log("player one inflicts this damage: ", player1Damage);
-//     var orcDamage = orc.weapon.attackDamage();
-//     console.log("orc inflicts this damage: ", orcDamage);
+//     var player1damageBonus = playerOne.weapon.attackdamageBonus();
+//     console.log("player one inflicts this damageBonus: ", player1damageBonus);
+//     var orcdamageBonus = orc.weapon.attackdamageBonus();
+//     console.log("orc inflicts this damageBonus: ", orcdamageBonus);
 //     console.log("orc health: ", orc.health);
 //     console.log("human health: ",playerOne.health);
-//     orc.health = orc.health - player1Damage;
-//     playerOne.health = playerOne.health - orcDamage;
+//     orc.health = orc.health - player1damageBonus;
+//     playerOne.health = playerOne.health - orcdamageBonus;
 //     console.log("The orc's health is now: ", orc.health);
 //     console.log(playerOne.playerName, "'s health is now: ", playerOne.health);
-//     $("#inputPlayerAttack").text(playerOne.playerName + playerOne.weapon.toString(player1Damage));
+//     $("#inputPlayerAttack").text(playerOne.playerName + playerOne.weapon.toString(player1damageBonus));
 //     $("#inputPlayerAttack").append("<p>" + playerOne.playerName + "'s hitpoints are now " + playerOne.health + "." + "</p>");
 
-//     // $("#inputEnemyAttack").text(orc.weapon.toString(orcDamage));
-//     $("#inputEnemyAttack").text("Badguy Bill" + " hits with " + orc.weapon.name + " for " + orcDamage + "!")
+//     // $("#inputEnemyAttack").text(orc.weapon.toString(orcdamageBonus));
+//     $("#inputEnemyAttack").text("Badguy Bill" + " hits with " + orc.weapon.name + " for " + orcdamageBonus + "!")
 //     $("#inputEnemyAttack").append("<p>" + "Badguy Bill" + " hitpoints are now " + orc.health + "." + "</p>")
 //     finalCountdown(orc);
 //   });
