6993c4458614f9496df81cef2ce0258fa708633e
Thomas J. Shavor <thomasshavor@gmail.com>
2016-09-06 10:36:28 -0500

    prelim dropdowns working

--- javascripts/app.js
+++ javascripts/app.js
@@ -7,17 +7,33 @@ let playerOne= "";
 let playerTwo= "";
 
 // JQUERY event listeners for interacting with the DOM:
-var createMechaOne= function(put click event here...){
+// var createMechaOne= function(put click event here...){
 
 // User 1 Text Entry/Submit Button (new version): works!
-$(document).on("click", "#mecha1_SubmitButton", function(){
-playerOneName= $("#player1_TextInput").val();
+// $(document).on("click", "#mecha1_SubmitButton", function(){
+// playerOneName= $("#player1_TextInput").val();
 // console.log("player-name", playerOne);
 // console.log(event);
+// });
+
+
+//////example for drop down event listeners from web///////////
+$(".menuOne li a").click(function(){
+  $(this).parents(".dropdown").find('.btn').html($(this).text() + ' <span class="caret"></span>');
+  $(this).parents(".dropdown").find('.btn').val($(this).data('value'));
+  console.log("Fighter 1 Selected", this.text);
+});
+////////////////////////////////////////////////////
+$(".menuTwo li a").click(function(){
+  $(this).parents(".dropdown").find('.btn').html($(this).text() + ' <span class="caret"></span>');
+  $(this).parents(".dropdown").find('.btn').val($(this).data('value'));
+  console.log("Fighter 2 Selected", this.text);
 });
 
 
-/////MECHA Selection Buttons//////////
+
+
+
 // need to add assignment of mecha classes here as well
 
 $("#Gunen_Lagann").click(function(){
@@ -54,10 +70,10 @@ $("#Rocketeer").click(function(){
 });
 
 
-}
+// }
 
 ///////////////////////////////////////////////////
-var createMechaOne= function(put click event here...){
+var createMechaOne = function(puclickeventhere){
 
 // User 2 Text Entry/Submit Button (new version): works!
 $(document).on("click", "#mecha2_SubmitButton", function(){
@@ -122,6 +138,6 @@ $("#Rocketeer").click(function(){
 
 
 
-}
+
 
 
