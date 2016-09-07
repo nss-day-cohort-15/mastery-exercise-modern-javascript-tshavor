035ec6938318be4274cfa4e3cd053fab372905e2
Thomas J. Shavor <thomasshavor@gmail.com>
2016-09-07 15:16:23 -0500

    prototypal inheritance corrected

--- javascripts/app.js
+++ javascripts/app.js
@@ -13,11 +13,9 @@ let playerTwo= "";
 $(".menuOne li a").click(function(){
    $(this).parents(".dropdown").find('.btn').html($(this).text() + ' <span class="caret"></span>');
     $(this).parents(".dropdown").find('.btn').val($(this).data('value'));
-
-
   // this.text correctly indicates the robot selected!
-  console.log("Fighter 1 Selected", this.text);
-  console.dir(this);
+  // console.log("Fighter 1 Selected", this.text);
+  // console.dir(this);
   // should I fire this function here?
   var a= this.text;
   makeMechaOne(a)
@@ -27,7 +25,7 @@ $(".menuTwo li a").click(function(){
   $(this).parents(".dropdown").find('.btn').html($(this).text() + ' <span class="caret"></span>');
   $(this).parents(".dropdown").find('.btn').val($(this).data('value'));
   // this.text correctly indicates the robot selected!
-  console.log("Fighter 2 Selected", this.text);
+  // console.log("Fighter 2 Selected", this.text);
   var b= this.text;
   makeMechaTwo(b)
 });
@@ -37,143 +35,53 @@ $(document).on("click", "#fightButton", function(){
 console.log("You clicked FIGHT!");
 });
 
-// Click events to fire functions to create mechas:
-// $(".menuOne").click(makeMechaOne);
-// $('.menuTwo').click(makeMechaTwo);
-
-
-// WHAT DO I NEED TO PASSS INTO THIS FUNCTION TO GET IT TO WORK???
-function makeMechaOne (x) {
-console.log("makeMechaOne FIRED!");
-
+///////////functions to create robots (mecha)/////////////
+    function makeMechaOne (x) {
+    // console.log("makeMechaOne FIRED!");
         if ($("#player1_TextInput").val() !== ''){
-
         let playerOneName = $("#player1_TextInput").val();
-        console.log(playerOneName);
         playerOne = new Battledome.Combatants[x](playerOneName);
+        // debugger;
         console.log("playerOne", playerOne);
-        // // printMechaOneStats(fighter1);
-        // playerOne = true;
+        printMechaOneStats();
+        playerOne = true;
     }
 
-//Requirement to ensure input text is completed:
-
-    // else {
-    //     $('#fighter1stats').html("Enter a name for your fighter and then select its type");
-    // }
     };
 
       function makeMechaTwo (y) {
-          console.log("makeMechaTwo FIRED!");
-
-          if ($("#player2_TextInput").val() !== '' && $(this).text() !== "Pick something!!!"){
-          let text = $(this).text();
+          // console.log("makeMechaTwo FIRED!");
+          if ($("#player2_TextInput").val() !== ''){
+          let playerTwoName = $("#player2_TextInput").val();
           let playerTwo = new Battledome.Combatants[y](playerTwoName);
           console.log("playerTwo", playerTwo);
-          // mechaTwo = new Battledome.Combatants[text](userName1);
-          // // printMechaTwoStats(fighter1);
-          // playerTwo = true;
+          printMechaTwoStats();
+          playerTwo = true;
       }
       };
 
 
+//Functions to print fighters 1 and 2 stats to DOM. These are both called twice: when fighters are first created and during fight itself, in order to reflect changes in fighter stats as fight function runs.
 
+    function printMechaOneStats(x){
+          let MechaOneStats = $(playerOneName);
+          console.log(playerOne);
+          console.log(playerOne.name);
+          // let MechaOneStats= `${playerOne.name}`;
+          // $("#playerOneStats").prepend(${x.name});
 
+          $("#playerOneStats").html("Thom" + playerOneName);
 
+    };
 
-
-
-
-
-
-
-
-// need to add assignment of mecha classes here as well
-
-// $("#Gunen_Lagann").click(function(){
-//   console.log("You clicked Gunen_Lagann");
-//   // battledome.Combatants.Bipedal
-//   playerOne = new Battledome.Combatants.Gunen_Lagann(playerOneName)
-//   // playerTwo = new Battledome.Combatants.Gunen_Lagann(playerTwoName)
-//   console.log(playerOne, playerTwo);
-// });
-
-// $("#Gundam_Unicorn").click(function(){
-//   playerOne = new Battledome.Combatants.Gundam_Unicorn(playerOneName)
-//   console.log(playerOne);
-// });
-
-// $("#Scout").click(function(){
-//   playerOne = new Battledome.Combatants.Scout(playerOneName)
-//   console.log(playerOne);
-// });
-
-// $("#Vulture").click(function(){
-//   playerOne = new Battledome.Combatants.Vulture(playerOneName)
-//   console.log(playerOne);
-// });
-
-// $("#Brawler").click(function(){
-//   playerOne = new Battledome.Combatants.Brawler(playerOneName)
-//   console.log(playerOne);
-// });
-
-// $("#Rocketeer").click(function(){
-//   playerOne = new Battledome.Combatants.Rocketeer(playerOneName)
-//   console.log(playerOne);
-// });
-
-
-// }
-
-
-
-// $("#Gunen_Lagann").click(function(){
-//   console.log("You clicked Gunen_Lagann");
-//   // battledome.Combatants.Bipedal
-//   playerOne = new Battledome.Combatants.Gunen_Lagann(playerOneName)
-//   // playerTwo = new Battledome.Combatants.Gunen_Lagann(playerTwoName)
-//   console.log(playerOne, playerTwo);
-// });
-
-// $("#Gundam_Unicorn").click(function(){
-//   playerOne = new Battledome.Combatants.Gundam_Unicorn(playerOneName)
-//   console.log(playerOne);
-// });
-
-// $("#Scout").click(function(){
-//   playerOne = new Battledome.Combatants.Scout(playerOneName)
-//   console.log(playerOne);
-// });
-
-// $("#Vulture").click(function(){
-//   playerOne = new Battledome.Combatants.Vulture(playerOneName)
-//   console.log(playerOne);
-// });
-
-// $("#Brawler").click(function(){
-//   playerOne = new Battledome.Combatants.Brawler(playerOneName)
-//   console.log(playerOne);
-// });
-
-// $("#Rocketeer").click(function(){
-//   playerOne = new Battledome.Combatants.Rocketeer(playerOneName)
-//   console.log(playerOne);
-// });
-
-
-// }
-
-
-
-
-
-
-
-
-
-
-
+    function printMechaTwoStats(y){
+        let MechaTwoStats = $(playerOneName);
+          console.log(playerTwo);
+          console.log(playerTwo.name);
+          // let MechaTwoStats = `<div style="border: 1px dotted black; border-radius: 25px; padding: 10px; margin-top:10px; background-color: lightgrey;">${x.name} the ${x.title} weighs ${x.weight} pounds, would rather be ${x.preferences}, and obviously has a ${x.attitude} attitude. ${x.name} also commands ${x.indenturedServant}, who has kindly provided ${x.name} with <span style="color:red; font-weight: bolder">${x.hasCans}</span> cans.</div>`
+          // $("#playerTwoStats").html(MechaTwoStats);
+    };
+/////////////////////////////FIGHT FUNCTIONS BELOW///////////////////////
 
 
 
--- javascripts/quiz.js
+++ javascripts/quiz.js
@@ -11,32 +11,36 @@ battledome.Combatants = {};
 //set top of prototype chain (Mecha constructor)
 battledome.Combatants.Mecha = function() {
 
-    this.health = 0;
-    this.damageBonus = 0;
+    this.health = 10;
+    this.damage = 5;
     this.allowedModels = ["Gunen_Lagann", "Gundam_Unicorn", "Scout", "Vulture", "Rocketeer", "Brawler"];
-    this.weapon = null;
+    // this.weapon = null;
 };
 
 
 //next level down on chain: types (Bipedal/Drone/Combat Constructors)
 battledome.Combatants.Bipedal = function() {
-  this.health = 20;
-  this.damageBonus= 10
+  this.health += 20;
+  this.damage += 10;
 };
 battledome.Combatants.Bipedal.prototype = new battledome.Combatants.Mecha();
 
 battledome.Combatants.Drone = function() {
 
   this.name = "Drone";
-  this.health = this.healthBonus +5;
-  this.damageBonus = this.damageBonus +5;
+  this.health += 5;
+  this.damage += 5;
+  // this.healthBonus = this.healthBonus +5;
+  // this.damageBonus = this.damageBonus +5;
 };
 battledome.Combatants.Drone.prototype = new battledome.Combatants.Mecha();
 
 battledome.Combatants.Combat = function() {
   this.name = "Combat";
-  this.health = this.healthBonus + 10;
-  this.damageBonus = this.damageBonus +20;
+  this.health += 10;
+  this.damage += 20;
+  // this.healthBonus = this.healthBonus + 10;
+  // this.damageBonus = this.damageBonus +20;
 };
 battledome.Combatants.Combat.prototype = new battledome.Combatants.Mecha();
 
@@ -44,45 +48,57 @@ battledome.Combatants.Combat.prototype = new battledome.Combatants.Mecha();
 //Bipedal:
 battledome.Combatants.Gunen_Lagann = function(userName) {
   this.name = userName;
-  this.healthBonus = this.healthBonus + Math.floor(Math.random() * 6 + 1);
-  this.damageBonus = this.damageBonus + Math.floor(Math.random() * 8 + 1);
+  this.health += Math.floor(Math.random() * 6 + 1);
+  this.damage += Math.floor(Math.random() * 8 + 1);
+  // this.healthBonus = this.healthBonus + Math.floor(Math.random() * 6 + 1);
+  // this.damageBonus = this.damageBonus + Math.floor(Math.random() * 8 + 1);
 };
 battledome.Combatants.Gunen_Lagann.prototype = new battledome.Combatants.Bipedal();
 
 battledome.Combatants.Gundam_Unicorn = function(userName) {
   this.name = userName;
-  this.healthBonus = this.healthBonus + Math.floor(Math.random() * 7 + 7);
-  this.damageBonus = this.damageBonus + Math.floor(Math.random() * 9 + 1);
+  this.health += Math.floor(Math.random() * 7 + 7);
+  this.damage += Math.floor(Math.random() * 9 + 1);
+  // this.healthBonus = this.healthBonus + Math.floor(Math.random() * 7 + 7);
+  // this.damageBonus = this.damageBonus + Math.floor(Math.random() * 9 + 1);
 };
 battledome.Combatants.Gundam_Unicorn.prototype = new battledome.Combatants.Bipedal();
 ////////////////////////////////////////////////////////////////////////////////////
 // Drone:
 battledome.Combatants.Scout = function(userName) {
   this.name = userName;
-  this.healthBonus = this.healthBonus + Math.floor(Math.random() * 2 + 1);
-  this.damageBonus = this.damageBonus + Math.floor(Math.random() * 2 + 1);
+  this.health += Math.floor(Math.random() * 2 + 1);
+  this.damage += Math.floor(Math.random() * 2 + 1);
+  // this.healthBonus = this.healthBonus + Math.floor(Math.random() * 2 + 1);
+  // this.damageBonus = this.damageBonus + Math.floor(Math.random() * 2 + 1);
 };
 battledome.Combatants.Scout.prototype = new battledome.Combatants.Drone();
 
 battledome.Combatants.Vulture = function(userName) {
   this.name = userName;
-  this.healthBonus = this.healthBonus + Math.floor(Math.random() * 3 + 3);
-  this.damageBonus = this.damageBonus + Math.floor(Math.random() * 2 + 5);
+  this.health += Math.floor(Math.random() * 3 + 3);
+  this.damage += Math.floor(Math.random() * 2 + 5);
+  // this.healthBonus = this.healthBonus + Math.floor(Math.random() * 3 + 3);
+  // this.damageBonus = this.damageBonus + Math.floor(Math.random() * 2 + 5);
 };
 battledome.Combatants.Vulture.prototype = new battledome.Combatants.Drone();
 ///////////////////////////////////////////////////////////////////////////
 // Combat:
 battledome.Combatants.Rocketeer = function(userName) {
   this.name = userName;
-  this.healthBonus = this.healthBonus + Math.floor(Math.random() * 4 + 2);
-  this.damageBonus = this.damageBonus + Math.floor(Math.random() * 4 + 1);
+  this.health += Math.floor(Math.random() * 4 + 2);
+  this.damage += Math.floor(Math.random() * 4 + 1);
+  // this.healthBonus = this.healthBonus + Math.floor(Math.random() * 4 + 2);
+  // this.damageBonus = this.damageBonus + Math.floor(Math.random() * 4 + 1);
 };
 battledome.Combatants.Rocketeer.prototype = new battledome.Combatants.Combat();
 
 battledome.Combatants.Brawler = function(userName) {
   this.name = userName;
-  this.healthBonus = this.healthBonus + Math.floor(Math.random() * 5 + 3);
-  this.damageBonus = this.damageBonus + Math.floor(Math.random() * 4 + 4);
+  this.health += Math.floor(Math.random() * 5 + 3);
+  this.damage += Math.floor(Math.random() * 4 + 4);
+  // this.healthBonus = this.healthBonus + Math.floor(Math.random() * 5 + 3);
+  // this.damageBonus = this.damageBonus + Math.floor(Math.random() * 4 + 4);
 };
 battledome.Combatants.Brawler.prototype = new battledome.Combatants.Combat();
 ///////////////////////////////////////////////////////////////////////////
